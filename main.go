1dcc2324943beaadf32cb3b3c35ff7bbf5057395
parkjunho12 <ghdlwnsgh12@naver.com>
2021-03-08 15:20:46 +0900

    make post

--- main.go
+++ main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
-
+	appEnv := flag.String("app-env", os.Getenv("APP_HOME"), "app env")
+	flag.Parse()
 	e := echo.New()
+
+	if *appEnv == "" {
+		*appEnv = `./`
+	}
 
 	e.GET("/say", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, "hello")
 	})
+	e.POST("/echo", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "hello")
+	})
+
 	e.Logger.Fatal(e.Start(":2020"))
 	fmt.Print("sdf")
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+		for {
+			signal := <-signalChan
+
+			switch signal {
+
+			case syscall.SIGHUP:
+				os.Exit(0)
+			case syscall.SIGINT:
+				os.Exit(0)
+			case syscall.SIGTERM:
+				os.Exit(0)
+			default:
+				//				os.Exit(0)
+			}
+		}
+	}()
 }
